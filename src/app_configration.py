0a6c1c7ff09128c402f0e249b71ea4b6f9cfea8d
Sanjay Saini <Sanjay Saini@SanjaySaini>
2018-07-11 09:50:12 +0530

    add superadmin email

--- src/app_configration.py
+++ src/app_configration.py
@@ -10,10 +10,10 @@ config={
     # send error emails to developers
     'send_mail_developer': True,
     'app_lang': 'en',
-    'super_admin': ['maxprolimited@gmail.com',
+    'super_admin': ['care4growth@gmail.com',
                     'sainisanjeet3@gmail.com',
                     'ballucorp@gmail.com',
-                     'balvinder.kumar913@gmail.com'],
+                    'balvinder.kumar913@gmail.com'],
     
     }
  
